78ffa6051e6958bf2c9e6a5d4da4fccc91ab3781
allen7u@GitKraken <allen7u@gmail.com>
2022-07-30 22:46:38 +0800

    add line break & unescape html

--- note_to_json_daily.py
+++ note_to_json_daily.py
@@ -13,12 +13,14 @@
 
 import os, re, json
 import meilisearch
+import html
 
-index_id = 'daily'
-index_id = 'all'
-index_id = 'all_line_break'
-id_num = 0
-records_list = []
+daily_index_id = 'daily'
+linely_index_id = 'linely'
+daily_id_num = 0
+linely_id_num = 0
+daily_list = []
+lines_list = []
 path = 'F:\\Anaconda_Play\\小米便签批量导出\\2022-7-29-2\\sub'
 path = 'F:\\Anaconda_Play\\小米便签批量导出\\2022-7-29-2\\'
 for file in os.listdir(path):
@@ -27,43 +29,56 @@ for file in os.listdir(path):
         continue
     with open(path + '\\' + file, 'r', encoding='utf-8') as f:
         note_content = f.read()
-        # print(note_content)
-    content_ = re.sub(r'(\d{1,2}月\d{1,2}日)','$$$\n'+r'\1',note_content)
-    # print(content_)
-    # segments = re.split('$$$','123 $$$ 456')
-    records = content_.split('$$$\n')
+    # Convert all named and numeric character references (e.g. &gt;, &#62;, &#x3e;) in the string s to the corresponding Unicode characters
+    note_content = html.unescape(note_content)
 
-    # pattern = re.compile(r'\d{,2}月\d{,2}日[\w\W]+?(?=\d{,2}月\d{,2}日)')
-    # records = re.findall(pattern, note_content)
-    # print(records)
-
-    for record in records:
-        # print(record)
+    note_content = re.sub(r'(\d{1,2}月\d{1,2}日)','$$$\n'+r'\1',note_content)
+    
+    for day in note_content.split('$$$\n'):
+        # print(day)
         pattern = re.compile(r'\d{,2}月\d{,2}日')
-        date = re.findall(pattern, record)
+        date = re.findall(pattern, day)
         if not date:
-            continue
-        date = re.findall(pattern, record)[0]
-        record = re.sub(r'\n','__LINEBREAK__', record)
+            continue # 如果没有日期，则跳过
+        date = re.findall(pattern, day)[0] # 取出日期
+        day = re.sub(r'\n','__LINEBREAK__', day) # 替换换行符
 
-        records_list.append({
-            'id': id_num,
+        daily_list.append({
+            'id': daily_id_num,
             'date':date,
             'file_name': file,
-            'content':record})
-            # 'content':'<div>' + record + '</div>'})
-        id_num += 1
+            'content':day})
+            # 'content':'<div>' + day + '</div>'})
+        daily_id_num += 1
 
-# print(records_list)
-    
-with open( index_id + '.json','w', encoding = 'utf-8') as f:
-    json.dump(records_list, f, indent= 4, ensure_ascii=False)
+        for line in day.split('__LINEBREAK__'): 
+            lines_list.append({     
+                'id': linely_id_num,        
+                'day_index': daily_id_num - 1,          
+                'date':date,            
+                'file_name': file,  
+                'content':line})    
+            linely_id_num += 1  
 
-client = meilisearch.Client('http://127.0.0.1:7700')
-json_file = open( index_id + '.json', encoding = 'utf-8')
-data = json.load(json_file)
-# print(data)
-client.index( index_id ).add_documents(data)
+# print(daily_list) 
+client = meilisearch.Client('http://127.0.0.1:7700')      
+
+with open( daily_index_id + '.json','w', encoding = 'utf-8') as f:            
+    json.dump(daily_list, f, indent= 4, ensure_ascii=False)     
+
+with open( daily_index_id + '.json', encoding = 'utf-8')  as f:     
+    data = json.load(f)     
+    client.index( daily_index_id ).add_documents(data)  
+
+with open( linely_index_id + '.json','w', encoding = 'utf-8') as f:     
+    json.dump(lines_list, f, indent= 4, ensure_ascii=False)     
+
+with open( linely_index_id + '.json', encoding = 'utf-8')  as f:        
+    data = json.load(f)         
+    client.index( linely_index_id ).add_documents(data)     
+
+
+
 
 
 #参考，备用：
